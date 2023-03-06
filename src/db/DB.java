9fc0214c57a8e815767abcdb5390aba2350e7df0
Joao Gabriel <jgbrlmotta@gmail.com>
2023-03-06 18:42:51 -0300

    findById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -10,17 +10,17 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	
+
 	private static Connection conn = null;
 	
-	public static Connection getConnectio() {
-		if(conn == null) {
+	public static Connection getConnection() {
+		if (conn == null) {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
-				conn = DriverManager.getConnection(url,props);
+				conn = DriverManager.getConnection(url, props);
 			}
-			catch(SQLException e) {
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
@@ -28,26 +28,28 @@ public class DB {
 	}
 	
 	public static void closeConnection() {
-		if(conn != null) {
-				try {
+		if (conn != null) {
+			try {
 				conn.close();
-			}
-			catch(SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
+	
 	private static Properties loadProperties() {
-		try(FileInputStream fs  = new FileInputStream("db.properties")){
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		}catch(IOException e ) {
+		}
+		catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
+	
 	public static void closeStatement(Statement st) {
-		if(st !=null) {
+		if (st != null) {
 			try {
 				st.close();
 			} catch (SQLException e) {
@@ -55,8 +57,9 @@ public class DB {
 			}
 		}
 	}
+
 	public static void closeResultSet(ResultSet rs) {
-		if(rs !=null) {
+		if (rs != null) {
 			try {
 				rs.close();
 			} catch (SQLException e) {
